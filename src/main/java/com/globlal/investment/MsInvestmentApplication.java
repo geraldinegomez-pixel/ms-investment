5819b94eb2faeee584a9ccb24e89ae7f69061c51
Geraldine Gomez <geraldine.gomez@global66.com>
2025-12-18 17:35:24 -0500

    chore: setup project structure

---
+++ src/main/java/com/globlal/investment/MsInvestmentApplication.java
@@ -0,0 +1,13 @@
+package com.globlal.investment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsInvestmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsInvestmentApplication.class, args);
+	}
+
+}
